788369e75bbe8cc356c47cd5b16146c45913b30d
gabderakhmanov <gabderakhmanov@gmail.com>
2017-12-31 17:04:02 +0300

    code refactoring

--- src/com/max/builder/Starter.java
+++ src/com/max/builder/Starter.java
@@ -4,10 +4,12 @@ public class Starter {
 
     public static void main(String[] args){
 
-        StudentInfoBuilder builder = new StudentInfoBuilder();
-        StudentInfo stInfo = builder.firstName("Max").lastName("Gee").build();
+        StudentInfo studentInfo = new StudentInfoBuilder()
+                .firstName("Max")
+                .lastName("Gee")
+                .build();
 
-        System.out.println(stInfo.getFirstName() + stInfo.getLastName());
+        System.out.println(studentInfo.getFirstName() + studentInfo.getLastName());
     }
 
 }
